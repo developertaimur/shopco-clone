1c0658220eca4adc3a097ef34873917e64c6d0f6
Muhammad Taimur <muhammadtaimur2001@gmail.com>
2026-06-09 01:12:50 +0500

    Create product detail page frontend

--- js/components.js
+++ js/components.js
@@ -1,26 +1,50 @@
-// Load Navbar Component
-fetch("components/navbar.html")
-  .then(function(response) {
-    return response.text();
-  })
-  .then(function(data) {
-    document.getElementById("navbar-container").innerHTML = data;
-  });
+// 
 
-  // Load Newsletter Component
-fetch("components/newsletter.html")
-  .then(function(response) {
-    return response.text();
-  })
-  .then(function(data) {
-    document.getElementById("newsletter-container").innerHTML = data;
-  });
+// Components path fix
+// Agar page "pages" folder ke andar hai to ../ use hoga
+// Agar index.html root me hai to normal path use hoga
 
-  // Load Footer Component
-fetch("components/footer.html")
-  .then(function(response) {
-    return response.text();
-  })
-  .then(function(data) {
-    document.getElementById("footer-container").innerHTML = data;
-  });
+let componentPath = "";
+
+if (window.location.pathname.includes("/pages/")) {
+  componentPath = "../";
+}
+
+// Load Navbar
+const navbarContainer = document.getElementById("navbar-container");
+
+if (navbarContainer) {
+  fetch(componentPath + "components/navbar.html")
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(data) {
+      navbarContainer.innerHTML = data;
+    });
+}
+
+// Load Newsletter
+const newsletterContainer = document.getElementById("newsletter-container");
+
+if (newsletterContainer) {
+  fetch(componentPath + "components/newsletter.html")
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(data) {
+      newsletterContainer.innerHTML = data;
+    });
+}
+
+// Load Footer
+const footerContainer = document.getElementById("footer-container");
+
+if (footerContainer) {
+  fetch(componentPath + "components/footer.html")
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(data) {
+      footerContainer.innerHTML = data;
+    });
+}
